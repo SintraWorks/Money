a19a7d46d27a348510eaf75c6349193d0cd0edd8
Antonio Nunes <antonio.nunes@maxdoro.nl>
2023-05-27 05:48:42 +0200

    Initial Commit

---
+++ Tests/MoneyTests/MoneyTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Money
+
+final class MoneyTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Money().text, "Hello, World!")
+    }
+}
